36507ea91ca024a63455ce4178253b6ada6c222f
emilymeng <eeng@seattletimes.com>
2021-04-07 18:55:09 -0700

    svg recoded
    adjusted code in svg to have correct styling

--- src/js/main.js
+++ src/js/main.js
@@ -166,8 +166,6 @@ searchBox.addEventListener("keyup", searchListener);
 window.onscroll = function() {fixNav()};
 detectIE();
 
-
-
 if ($(window).width() < 700) {
    $('.desktop').hide();
    $('.mobile').show();
@@ -214,8 +212,43 @@ $( ".collapse" ).click(function() {
 
 });
 
+//for the hotspot svg illo anchor tags
+var dot = require("./lib/dot");
+
+var template = dot.compile(require("./_info.html"));
+
+var qsa = s => Array.prototype.slice.call(document.querySelectorAll(s));
+
+qsa(".st-group").forEach(function(group) {
+  group.addEventListener("click", function(e) {
+    document.querySelector(".info").innerHTML = template(data[e.target.parentElement.id]);
+    if (document.querySelector(".selected")) document.querySelector(".selected").classList.remove("selected");
+    e.target.parentElement.classList.add("selected");
+  });
+});
 
 
+var options = Object.keys(window.data);
 
+qsa(".goto").forEach(function(btn) {
+	btn.addEventListener("click", function(e) {
+
+		var selected = document.querySelector(".selected");
+
+		var selectedIndex = selected ? options.indexOf(selected.id) : -1;
+		if (e.target.classList.contains("back")) {
+			selectedIndex = Math.max(0, selectedIndex - 1);
+		} else {
+			selectedIndex = Math.min(options.length - 1, selectedIndex + 1);
+		}
+
+		$(".goto.back").toggleClass("disabled", selectedIndex == 0);
+    $(".goto.next").toggleClass("disabled", selectedIndex == options.length - 1);
+
+		document.querySelector(".details").innerHTML = template(data[options[selectedIndex]]);
+    if (document.querySelector(".selected")) document.querySelector(".selected").classList.remove("selected");
+    document.querySelector("#" + options[selectedIndex]).classList.add("selected");
+	});
+});
 
 
